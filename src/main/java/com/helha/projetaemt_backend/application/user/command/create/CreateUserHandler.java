268798982126e0ab02f1818e75376d33cee8d483
Sayzzi <zarunixopl@hotmail.com>
2026-01-19 11:19:49 +0100

    continuation du command create pour le user

--- src/main/java/com/helha/projetaemt_backend/application/user/command/create/CreateUserHandler.java
+++ src/main/java/com/helha/projetaemt_backend/application/user/command/create/CreateUserHandler.java
@@ -1,4 +1,4 @@
 package com.helha.projetaemt_backend.application.user.command.create;
 
-public class CreateUserHandler {
+public class CreateUserHandler implements ICommandHagindler<CreateUserInput, CreateUserOutput>{
 }
