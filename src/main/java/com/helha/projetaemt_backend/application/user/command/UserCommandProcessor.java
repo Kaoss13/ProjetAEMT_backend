b84bf718b57b59b656e339370a4a1e6617a87770
Sayzzi <zarunixopl@hotmail.com>
2026-01-19 12:17:00 +0100

    ajout du commandProcessor

--- src/main/java/com/helha/projetaemt_backend/application/user/command/UserCommandProcessor.java
+++ src/main/java/com/helha/projetaemt_backend/application/user/command/UserCommandProcessor.java
@@ -1,4 +1,26 @@
 package com.helha.projetaemt_backend.application.user.command;
 
+import com.helha.projetaemt_backend.application.user.command.create.CreateUserHandler;
+import com.helha.projetaemt_backend.application.user.command.create.CreateUserInput;
+import com.helha.projetaemt_backend.application.user.command.create.CreateUserOutput;
+import com.helha.projetaemt_backend.application.user.command.login.LoginUserHandler;
+import com.helha.projetaemt_backend.application.user.command.login.LoginUserInput;
+import com.helha.projetaemt_backend.application.user.command.login.LoginUserOutput;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserCommandProcessor {
+
+    public final CreateUserHandler createUserHandler;
+    public final LoginUserHandler loginUserHandler;
+
+    public UserCommandProcessor(CreateUserHandler createUserHandler, LoginUserHandler loginUserHandler) {
+        this.createUserHandler = createUserHandler;
+        this.loginUserHandler = loginUserHandler;
+    }
+
+
+
+
+
 }
--- src/main/java/com/helha/projetaemt_backend/controllers/user/UserCommandController.java
+++ src/main/java/com/helha/projetaemt_backend/controllers/user/UserCommandController.java
@@ -1,9 +1,8 @@
 package com.helha.projetaemt_backend.controllers.user;
 
-import com.helha.projetaemt_backend.application.user.command.create.CreateUserHandler;
+import com.helha.projetaemt_backend.application.user.command.UserCommandProcessor;
 import com.helha.projetaemt_backend.application.user.command.create.CreateUserInput;
 import com.helha.projetaemt_backend.application.user.command.create.CreateUserOutput;
-import com.helha.projetaemt_backend.application.user.command.login.LoginUserHandler;
 import com.helha.projetaemt_backend.application.user.command.login.LoginUserInput;
 import com.helha.projetaemt_backend.application.user.command.login.LoginUserOutput;
 import jakarta.validation.Valid;
@@ -16,23 +15,21 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "*")
 public class UserCommandController {
 
-    private final CreateUserHandler createUserHandler;
-    private final LoginUserHandler loginUserHandler;
+    private final UserCommandProcessor userCommandProcessor;
 
-    public UserCommandController(CreateUserHandler createUserHandler, LoginUserHandler loginUserHandler) {
-        this.createUserHandler = createUserHandler;
-        this.loginUserHandler = loginUserHandler;
+    public UserCommandController(UserCommandProcessor userCommandProcessor) {
+        this.userCommandProcessor = userCommandProcessor;
     }
 
     @PostMapping
     public ResponseEntity<CreateUserOutput> createUser(@Valid @RequestBody CreateUserInput input) {
-        CreateUserOutput output = createUserHandler.handle(input);
+        CreateUserOutput output = userCommandProcessor.create(input);
         return ResponseEntity.status(HttpStatus.CREATED).body(output);
     }
 
     @PostMapping("/login")
     public ResponseEntity<LoginUserOutput> login(@Valid @RequestBody LoginUserInput input) {
-        LoginUserOutput output = loginUserHandler.handle(input);
+        LoginUserOutput output = userCommandProcessor.login(input);
         return ResponseEntity.ok(output);
     }
 }
