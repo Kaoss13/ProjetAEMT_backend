80a57e463f731530aedaf6cfb580fcdc818b9bbd
Sayzzi <zarunixopl@hotmail.com>
2026-01-19 10:59:56 +0100

    correction user infra

--- src/main/java/com/helha/projetaemt_backend/infrastructure/user/DbUser.java
+++ src/main/java/com/helha/projetaemt_backend/infrastructure/user/DbUser.java
@@ -1,14 +1,11 @@
 package com.helha.projetaemt_backend.infrastructure.user;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class DbUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
