613b227bc8015bc56b1921d7ba5383721f6dea9c
Sayzzi <zarunixopl@hotmail.com>
2026-01-19 15:41:22 +0100

    cors update

--- src/main/java/com/helha/projetaemt_backend/infrastructure/utils/CorsConfig.java
+++ src/main/java/com/helha/projetaemt_backend/infrastructure/utils/CorsConfig.java
@@ -10,6 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
                 .allowedHeaders("*");
     }
 }
