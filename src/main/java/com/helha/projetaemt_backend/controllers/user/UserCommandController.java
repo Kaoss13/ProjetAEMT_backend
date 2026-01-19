6c9a4789b37af838dead77cf594a1635626d747b
Sayzzi <zarunixopl@hotmail.com>
2026-01-19 12:54:43 +0100

    modification du usercommandcontroller afin de respecter la convention du grp

--- src/main/java/com/helha/projetaemt_backend/controllers/user/UserCommandController.java
+++ src/main/java/com/helha/projetaemt_backend/controllers/user/UserCommandController.java
@@ -5,14 +5,20 @@ import com.helha.projetaemt_backend.application.user.command.create.CreateUserIn
 import com.helha.projetaemt_backend.application.user.command.create.CreateUserOutput;
 import com.helha.projetaemt_backend.application.user.command.login.LoginUserInput;
 import com.helha.projetaemt_backend.application.user.command.login.LoginUserOutput;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/users")
-@CrossOrigin(origins = "*")
 public class UserCommandController {
 
     private final UserCommandProcessor userCommandProcessor;
@@ -21,15 +27,38 @@ public class UserCommandController {
         this.userCommandProcessor = userCommandProcessor;
     }
 
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    headers = @Header(
+                            name = "Location",
+                            description = "Location of created resource")),
+            @ApiResponse(responseCode = "400",
+                    content = @Content(schema = @Schema(implementation = org.springframework.http.ProblemDetail.class))
+            )
+    })
     @PostMapping
-    public ResponseEntity<CreateUserOutput> createUser(@Valid @RequestBody CreateUserInput input) {
-        CreateUserOutput output = userCommandProcessor.create(input);
-        return ResponseEntity.status(HttpStatus.CREATED).body(output);
+    public ResponseEntity<CreateUserOutput> create(@Valid @RequestBody CreateUserInput input) {
+        CreateUserOutput output = userCommandProcessor.createUserHandler.handle(input);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(output.id)
+                .toUri();
+        return ResponseEntity
+                .created(location)
+                .body(output);
     }
 
+    @ApiResponses({
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400",
+                    content = @Content(schema = @Schema(implementation = org.springframework.http.ProblemDetail.class))
+            )
+    })
     @PostMapping("/login")
     public ResponseEntity<LoginUserOutput> login(@Valid @RequestBody LoginUserInput input) {
-        LoginUserOutput output = userCommandProcessor.login(input);
+        LoginUserOutput output = userCommandProcessor.loginUserHandler.handle(input);
         return ResponseEntity.ok(output);
     }
 }
