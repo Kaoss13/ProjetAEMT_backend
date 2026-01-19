05f33373650e6ab3fce4f950106782ca98ab3df0
Kaoss13 <wattieznic@gmail.com>
2026-01-19 09:23:09 +0100

    First commit

---
+++ src/main/java/com/helha/projetaemt_backend/ProjetAemtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.helha.projetaemt_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetAemtBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetAemtBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/helha/projetaemt_backend/ProjetAemtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helha.projetaemt_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetAemtBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
