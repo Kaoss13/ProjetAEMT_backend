48e1ed97854e517f09b97f480134cb21ce851960
Sayzzi <zarunixopl@hotmail.com>
2026-01-19 12:05:30 +0100

    ajout annotation lombok

--- src/main/java/com/helha/projetaemt_backend/infrastructure/user/IUserRepository.java
+++ src/main/java/com/helha/projetaemt_backend/infrastructure/user/IUserRepository.java
@@ -1,9 +1,11 @@
 package com.helha.projetaemt_backend.infrastructure.user;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface IUserRepository extends CrudRepository<DbUser, Long> {
 
     Optional<DbUser> findByUserName(String userName);
