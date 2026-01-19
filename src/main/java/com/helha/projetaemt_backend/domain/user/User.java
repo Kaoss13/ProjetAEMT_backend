4f42db6b3ac0696f4ddb652c0b09c27b030ac1b3
Sayzzi <zarunixopl@hotmail.com>
2026-01-19 10:34:35 +0100

    user ajouté correctment

--- src/main/java/com/helha/projetaemt_backend/domain/user/User.java
+++ src/main/java/com/helha/projetaemt_backend/domain/user/User.java
@@ -1,7 +1,24 @@
 package com.helha.projetaemt_backend.domain.user;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
 public class User {
-    public int Id;
-    public String user_name;
-    public String hash_password;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int id;
+
+    @Column(name = "user_name")
+    public String userName;
+
+    @Column(name = "hash_password")
+    public String hashPassword;
 }
