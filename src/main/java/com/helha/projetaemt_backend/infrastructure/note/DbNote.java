83627a1e0d15089ca4cd54e29aed4318c1c3de6e
Kaoss13 <wattieznic@gmail.com>
2026-01-19 10:42:13 +0100

    Domain note

--- src/main/java/com/helha/projetaemt_backend/infrastructure/note/DbNote.java
+++ src/main/java/com/helha/projetaemt_backend/infrastructure/note/DbNote.java
@@ -1,4 +1,30 @@
 package com.helha.projetaemt_backend.infrastructure.note;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notes")
 public class DbNote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_user", nullable = false)
+    public User user;
+
+    @ManyToOne
+    @JoinColumn(name = "id_folder")
+    public Folder folder;
+
+    public String title;
+    public String content;
+    public LocalDateTime createdAt;
+    public LocalDateTime updatedAt;
+    public int syzeBytes;
+    public int lineCount;
+    public int wordCount;
+    public int charCount;
 }
