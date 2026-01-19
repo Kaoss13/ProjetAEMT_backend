ecdd37be56773625e6a75fc1c6de4d597b580abe
Kaoss13 <wattieznic@gmail.com>
2026-01-19 21:09:32 +0100

    Correction bug NoteMapper

--- src/main/java/com/helha/projetaemt_backend/infrastructure/note/INoteRepository.java
+++ src/main/java/com/helha/projetaemt_backend/infrastructure/note/INoteRepository.java
@@ -1,5 +1,6 @@
 package com.helha.projetaemt_backend.infrastructure.note;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +9,6 @@ import java.util.List;
 
 @Repository
 public interface INoteRepository extends CrudRepository<DbNote, Integer> {
+    @EntityGraph(attributePaths = {"user", "folder"})
     List<DbNote> findByFolderId(int id);
 }
