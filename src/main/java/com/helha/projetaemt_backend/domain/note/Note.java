66d86dc04cc3dc2a53a49a8f97d5c2f5b20db2d8
Kaoss13 <wattieznic@gmail.com>
2026-01-19 10:37:34 +0100

    Domain note

--- src/main/java/com/helha/projetaemt_backend/domain/note/Note.java
+++ src/main/java/com/helha/projetaemt_backend/domain/note/Note.java
@@ -1,4 +1,23 @@
 package com.helha.projetaemt_backend.domain.note;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
 public class Note {
+
+    private int idUser;
+    private int idFolder;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private int syzeBytes;
+    private int lineCount;
+    private int wordCount;
+    private int charCount;
+
 }
