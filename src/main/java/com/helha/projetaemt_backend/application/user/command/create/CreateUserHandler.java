14cab6b3582092873c9cc130435b273b5ac95d71
Sayzzi <zarunixopl@hotmail.com>
2026-01-19 11:54:31 +0100

    ajout de la route pour crée et se connecter

--- src/main/java/com/helha/projetaemt_backend/application/user/command/create/CreateUserHandler.java
+++ src/main/java/com/helha/projetaemt_backend/application/user/command/create/CreateUserHandler.java
@@ -1,13 +1,14 @@
 package com.helha.projetaemt_backend.application.user.command.create;
 
 import com.helha.projetaemt_backend.application.utils.ICommandHandler;
+import com.helha.projetaemt_backend.infrastructure.user.DbUser;
 import com.helha.projetaemt_backend.infrastructure.user.IUserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CreateUserHandler implements ICommandHandler<CreateUserInput, CreateUserOutput> {
-
-
 
     private final IUserRepository userRepository;
     private final ModelMapper modelMapper;
@@ -19,10 +20,22 @@ public class CreateUserHandler implements ICommandHandler<CreateUserInput, Creat
         this.passwordEncoder = passwordEncoder;
     }
 
-
-
     @Override
     public CreateUserOutput handle(CreateUserInput input) {
-        return null;
+        if (userRepository.existsByUserName(input.userName)) {
+            throw new IllegalArgumentException("Un utilisateur avec ce nom existe déjà");
+        }
+
+        DbUser dbUser = new DbUser();
+        dbUser.userName = input.userName;
+        dbUser.hashPassword = passwordEncoder.encode(input.password);
+
+        DbUser savedUser = userRepository.save(dbUser);
+
+        CreateUserOutput output = new CreateUserOutput();
+        output.id = savedUser.id;
+        output.userName = savedUser.userName;
+
+        return output;
     }
 }
--- src/main/java/com/helha/projetaemt_backend/application/user/command/login/LoginUserHandler.java
+++ src/main/java/com/helha/projetaemt_backend/application/user/command/login/LoginUserHandler.java
@@ -1,4 +1,35 @@
 package com.helha.projetaemt_backend.application.user.command.login;
 
-public class LoginUserHandler {
+import com.helha.projetaemt_backend.application.utils.ICommandHandler;
+import com.helha.projetaemt_backend.infrastructure.user.DbUser;
+import com.helha.projetaemt_backend.infrastructure.user.IUserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginUserHandler implements ICommandHandler<LoginUserInput, LoginUserOutput> {
+
+    private final IUserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public LoginUserHandler(IUserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public LoginUserOutput handle(LoginUserInput input) {
+        DbUser dbUser = userRepository.findByUserName(input.userName)
+                .orElseThrow(() -> new IllegalArgumentException("Nom d'utilisateur ou mot de passe incorrect"));
+
+        if (!passwordEncoder.matches(input.password, dbUser.hashPassword)) {
+            throw new IllegalArgumentException("Nom d'utilisateur ou mot de passe incorrect");
+        }
+
+        LoginUserOutput output = new LoginUserOutput();
+        output.id = dbUser.id;
+        output.userName = dbUser.userName;
+
+        return output;
+    }
 }
