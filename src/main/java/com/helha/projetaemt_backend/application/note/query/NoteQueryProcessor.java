8cdcad1d5a1c70f39bfa660ab5c2d7cdb37b051c
Kaoss13 <wattieznic@gmail.com>
2026-01-20 14:53:12 +0100

    Exportation PDF + lien externe ok

--- src/main/java/com/helha/projetaemt_backend/application/note/query/NoteQueryProcessor.java
+++ src/main/java/com/helha/projetaemt_backend/application/note/query/NoteQueryProcessor.java
@@ -1,5 +1,6 @@
 package com.helha.projetaemt_backend.application.note.query;
 
+import com.helha.projetaemt_backend.application.exportation.PdfExportHandler;
 import com.helha.projetaemt_backend.application.note.query.getbyid.GetByIdNoteHandler;
 import com.helha.projetaemt_backend.application.note.query.getbyidfolder.GetByIdFolderNoteHandler;
 import com.helha.projetaemt_backend.application.note.query.getbyidfolder.GetByIdFolderNoteOutput;
@@ -9,9 +10,11 @@ import org.springframework.stereotype.Service;
 public class NoteQueryProcessor {
     public final GetByIdNoteHandler getByIdNoteHandler;
     public final GetByIdFolderNoteHandler getByIdFolderNoteHandler;
+    public final PdfExportHandler pdfExportHandler;
 
-    public NoteQueryProcessor(GetByIdNoteHandler getByIdNoteHandler, GetByIdFolderNoteHandler getByIdFolderNoteHandler) {
+    public NoteQueryProcessor(GetByIdNoteHandler getByIdNoteHandler, GetByIdFolderNoteHandler getByIdFolderNoteHandler, PdfExportHandler pdfExportHandler) {
         this.getByIdNoteHandler = getByIdNoteHandler;
         this.getByIdFolderNoteHandler = getByIdFolderNoteHandler;
+        this.pdfExportHandler = pdfExportHandler;
     }
 }
