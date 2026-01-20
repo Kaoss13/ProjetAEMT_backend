37ef6f1c57b11934a91f21b0ab2710420bdcdbf7
NolhanFlrqq <la237094@student.helha.be>
2026-01-20 14:27:23 +0100

    getAll

---
+++ src/main/java/com/helha/projetaemt_backend/application/folder/query/getAll/GetAllFoldersWithNotesOutput.java
@@ -0,0 +1,25 @@
+package com.helha.projetaemt_backend.application.folder.query.getAll;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GetAllFoldersWithNotesOutput {
+
+    public static class FolderDto {
+        public int id;
+        public int userId;
+        public Integer id_parent_folder;
+        public String title;
+    }
+
+    public static class NoteDto {
+        public int id;
+        public int id_user;
+        public int id_folder;
+        public String title;
+        public String content;
+    }
+
+    public List<FolderDto> folders = new ArrayList<>();
+    public List<NoteDto> notes = new ArrayList<>();
+}
