419eadaea27a5a14b27f60c8876e7a484daa8d1f
Kaoss13 <wattieznic@gmail.com>
2026-01-20 09:57:54 +0100

    Ajout Data sur les inputs

--- src/main/java/com/helha/projetaemt_backend/application/note/command/create/CreateNoteInput.java
+++ src/main/java/com/helha/projetaemt_backend/application/note/command/create/CreateNoteInput.java
@@ -1,9 +1,11 @@
 package com.helha.projetaemt_backend.application.note.command.create;
 
 import lombok.Builder;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
+@Data
 @Builder
 public class CreateNoteInput {
 
