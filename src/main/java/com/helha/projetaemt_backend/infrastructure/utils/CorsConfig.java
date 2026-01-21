e69c87f7919289f0fc3b3e64694e4d2cc7ccf8a6
Kaoss13 <wattieznic@gmail.com>
2026-01-21 09:30:55 +0100

    Correction class cors

--- src/main/java/com/helha/projetaemt_backend/infrastructure/utils/CorsConfig.java
+++ src/main/java/com/helha/projetaemt_backend/infrastructure/utils/CorsConfig.java
@@ -1,17 +1,36 @@
 package com.helha.projetaemt_backend.infrastructure.utils;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Collections;
+
+import static java.util.Arrays.asList;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*")
-                .allowedOrigins("http://localhost:5173/", "http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("");
+    @Bean
+    public CorsFilter corsFilter() {
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        final CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+
+        config.setAllowedHeaders(asList("Origin", "Content-Type", "Accept"));
+
+        config.setAllowedMethods(asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+
     }
 }
